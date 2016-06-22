bcf536e051f4935cf68c4ef0149bf81e69ce2fef
reut.c <reut.c@supersonic.com>
2016-06-22 17:09:10 +0300

    login

--- PlatformApplication/app/src/main/java/com/platform/supersonic/platformapplication/MainActivity.java
+++ PlatformApplication/app/src/main/java/com/platform/supersonic/platformapplication/MainActivity.java
@@ -2,12 +2,33 @@ package com.platform.supersonic.platformapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bLogin;
+    EditText etUsername,etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
+
+        bLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bLogin:
+                // todo login
+                break;
+        }
     }
 }
