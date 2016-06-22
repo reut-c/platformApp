449afa014ad016d9ac80f5ac11d0e2ee33138147
reut.c <reut.c@supersonic.com>
2016-06-22 18:46:26 +0300

    first activity

---
+++ PlatformApplication/app/src/main/java/com/platform/supersonic/platformapplication/firstActivity.java
@@ -0,0 +1,43 @@
+package com.platform.supersonic.platformapplication;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FirstActivity extends BasicActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        isTokenValid();
+    }
+
+    private void isTokenValid(){
+        SharedPreferences settings = getSharedPreferences(USERDATA, 0);
+        String token = settings.getString(TOKEN, null);
+        if (token == null | token.length() == 0){ // if the user doesn't have token, move to the login page
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        } else {
+            String expirationDate = settings.getString(EXPIRATION_DATE,null);
+            if (expirationDate != null){
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                try {
+                    Date expireDate = dateFormat.parse(expirationDate);
+                    //return expireDate.after(new Date());
+                }
+                catch (Exception e){
+                    //return false;
+                }
+            }
+            else {
+                //return false;
+            }
+        }
+    }
+}
